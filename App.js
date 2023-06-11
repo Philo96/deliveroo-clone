e572709bc558fa899d4f69239e197e48c0b563d8
Philo96 <philopatearabdelfady96@gmail.com>
2023-06-11 20:17:45 +0300

    adding internal style sheets

--- App.js
+++ App.js
@@ -3,9 +3,18 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>This app is Basically made  by Philopatear Abdelmaseh</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#b72f2f',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
