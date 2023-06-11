bbcff641af683f4fcc0f6405d47bcc581cc80da0
Philo96 <philopatearabdelfady96@gmail.com>
2023-06-11 22:52:06 +0300

    edit code in 'HomeScreen.js' file

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useLayoutEffect} from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+import {useNavigation} from "@react-navigation/native";
 
 const HomeScreen = () => {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        })
+    }, [])
+
     return (
-        <View>
-            <Text className='text-red-500'>Home Screen</Text>
-        </View>
+        <SafeAreaView>
+            <View>
+                <Text className='text-red-500'>Home Screen</Text>
+            </View>
+        </SafeAreaView>
     );
 };
 
